a838b63ad02162be1fb37d9fef4b1d3c96b85595
Anika Bernstein <anikajb92@gmail.com>
2022-02-09 14:05:09 -0700

    experimented with ts types

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
 
-function doSomething() {
-  for (let i=0; i < 5; i++){
-    console.log(i)
-  }
-
-  console.log("Finally" + i); // outside of scope, error in compiler
-}
-
-doSomething();
+let count = 5;
+count = 'a';
