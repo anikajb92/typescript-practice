f5ec7ae8f706d37defd796a61f252364436c6a60
Anika Bernstein <anikajb92@gmail.com>
2022-02-10 19:43:00 -0700

    wrote an alternate to type declaration

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
 let a: number;
 let b: boolean;
 let c: string;
-let d: any;
+let d: any; //default 
 let e: number[] = [1,2,3];
 let f: any[] = [1, true, 'a', false];
 
@@ -12,3 +12,10 @@ const ColorBlue = 2;
 
 enum Color {Red = 0, Green = 1, Blue = 2};
 let background = Color.Red;
+
+let message; // any
+message = 'abc';
+// let endsWithC = message.endsWith('c');
+
+let endsWithC = (<string>message).endsWith('c');
+let alternativeWay = (message as string).endsWith('c');
